08936323db6ad77ec533a2a042d526e543438e3a
grantdbradley <gdb2@protonmail.com>
2021-03-09 16:55:11 -0500

    upload

--- xpt/fitter.py
+++ xpt/fitter.py
@@ -4,7 +4,48 @@ import gvar as gv
 import sys
 import os
 
-import fitter.special_functions as sf
+import non_analytic_functions as naf
+
+
+class Xi(lsqfit.MultiFitterModel):
+    def __init__(self,p):
+        self.p = p
+
+    def fitfcn_nlo_ct(self, p):
+        output = p['m_{xi,0}'] * (1 + p['d_a'] * (0.5* (p['a/w'])**2))
+
+    return output
+
+    def fitfcn_nlo_xpt(self, p):
+
+
+
+
+    
+
+    def fitfcn_n2lo_ct(self, p):     
+        output = ( 
+            + p['A_aa'] *xi['a'] *xi['a']
+            + p['A_al'] *xi['a'] *xi['l']
+            + p['A_as'] *xi['a'] *xi['s']
+            + p['A_ll'] *xi['l'] *xi['l']
+            + p['A_ls'] *xi['l'] *xi['s']
+            + p['A_ss'] *xi['s'] *xi['s']
+        )
+
+        if self.debug:
+            self.debug_table['n2lo_ct'] = output
+
+        return output
+
+    def fitfcn_n2lo_xpt(self,p):
+        
+
+
+
+    
+
+
 
 class fitter(object):
 
@@ -28,3 +69,334 @@ class fitter(object):
 
     def __str__(self):
         return str(self.fit)
+
+        @property
+    def fit(self):
+        if self._fit is None:
+            models = self._make_models()
+            y_data = {self.model_info['name'] : self.y}
+            prior = self._make_prior()
+
+            fitter = lsqfit.MultiFitter(models=models)
+            fit = fitter.lsqfit(data=y_data, prior=prior, fast=False, mopt=False)
+
+            self._fit = fit
+
+        return self._fit
+
+
+    #@property
+    def fit_interpolation(self, simultaneous=None):
+        if simultaneous is None:
+            simultaneous = self._simultaneous
+
+        if self._fit_interpolation is None or simultaneous != self._simultaneous:
+            self._simultaneous = simultaneous
+            #make_gvar = lambda g : gv.gvar(gv.mean(g), gv.sdev(g))
+            #y_data = make_gvar(1 / self.fit_data['a/w'])
+
+            make_gvar = lambda g : gv.gvar(gv.mean(g), gv.sdev(g))
+            if self.observable == 'w0':
+                data = {self.model_info['name']+'_interpolation' : 1 / make_gvar(self.fit_data['a/w']) }
+            elif self.observable == 't0':
+                data = {self.model_info['name']+'_interpolation' : make_gvar(self.fit_data['t/a^2']) }
+
+            if simultaneous:
+                data[self.model_info['name']] = self.y
+
+
+            models = self._make_models(interpolation=True, simultaneous=simultaneous)
+            prior = self._make_prior(interpolation=True, simultaneous=simultaneous)
+
+            fitter = lsqfit.MultiFitter(models=models)
+            fit = fitter.lsqfit(data=data, prior=prior, fast=False, mopt=False)
+            self._fit_interpolation = fit
+
+        return self._fit_interpolation
+
+        def _make_models(self, model_info=None, interpolation=False, y_data=None, simultaneous=False):
+        if model_info is None:
+            model_info = self.model_info.copy()
+
+        models = np.array([])
+        if interpolation:
+
+            model_info_interpolation = {
+                'name' : model_info['name'] + '_interpolation',
+                'chiral_cutoff': 'Fpi',
+                'order': 'n2lo',
+                'latt_ct': 'n2lo',
+                'include_log': False,
+                'include_log2': False,
+                'include_fv': False,
+                'include_alphas': False,
+                'exclude': []
+            }
+
+            datatag = model_info_interpolation['name']
+            models = np.append(models, model_interpolation(datatag=datatag, model_info=model_info_interpolation, ens_mapping=self._ensemble_mapping, observable=self.observable))
+            if not simultaneous:
+                return models
+
+        datatag = model_info['name']
+        models = np.append(models, model(datatag=datatag, model_info=model_info))
+
+        return models
+
+class Xi(lsqfit.MultiFitterModel):
+
+    def __init__(self, datatag, model_info, **kwargs):
+        super(model, self).__init__(datatag)
+
+
+    def fitfcn(self, p):
+        
+
+
+        # lo
+        output = p['c0']
+
+        if self.debug:
+            self.debug_table['lo_ct'] = output
+
+        # nlo
+        if self.model_info['order'] in ['nlo', 'n2lo', 'n3lo']:
+            output += self.fitfcn_nlo_ct(p, xi)
+            if self.model_info['include_alphas']:
+                output += self.fitfcn_nlo_latt_alphas(p, xi)
+                
+        elif self.model_info['latt_ct'] in ['nlo', 'n2lo', 'n3lo']: 
+            output += self.fitfcn_nlo_latt_ct(p, xi)
+            if self.model_info['include_alphas']:
+                output += self.fitfcn_nlo_latt_alphas(p, xi)
+
+        # n2lo 
+        if self.model_info['order'] in ['n2lo', 'n3lo']:
+            output += self.fitfcn_n2lo_ct(p, xi)
+            if self.model_info['include_log']:
+                output += self.fitfcn_n2lo_log(p, xi)
+
+        elif self.model_info['latt_ct'] in ['n2lo', 'n3lo']:
+            output += self.fitfcn_n2lo_latt_ct(p, xi)
+
+        # n3lo
+        if self.model_info['order'] in ['n3lo']:
+            output += self.fitfcn_n3lo_ct(p, xi)
+            if self.model_info['include_log']:
+                output += self.fitfcn_n3lo_log(p, xi)
+            if self.model_info['include_log2']:
+                output += self.fitfcn_n3lo_log_sq(p, xi)
+                
+        elif self.model_info['latt_ct'] in ['n3lo']:
+            output += self.fitfcn_n3lo_latt_ct(p, xi)
+
+
+
+        for key in self.model_info['exclude']:
+            del(p[key])
+
+        if debug:
+            #print(gv.tabulate(self.debug_table))
+            temp_string = ''
+            for key in self.debug_table:
+                temp_string +='  % .15f:  %s\n' %(gv.mean(self.debug_table[key]), key)
+            temp_string +='   -----\n'
+            temp_string +='  % .15f:  %s\n' %(gv.mean(output), 'total')
+            print(temp_string)
+
+        return output
+
+
+    def fitfcn_nlo_ct(self, p, xi):
+        output = p['A_l'] *xi['l'] + p['A_s'] *xi['s'] + p['A_a'] *xi['a']
+
+        if self.debug:
+            self.debug_table['nlo_ct'] = output
+
+        return output
+
+
+    def fitfcn_n2lo_ct(self, p, xi):     
+        output = ( 
+            + p['A_aa'] *xi['a'] *xi['a']
+            + p['A_al'] *xi['a'] *xi['l']
+            + p['A_as'] *xi['a'] *xi['s']
+            + p['A_ll'] *xi['l'] *xi['l']
+            + p['A_ls'] *xi['l'] *xi['s']
+            + p['A_ss'] *xi['s'] *xi['s']
+        )
+
+        if self.debug:
+            self.debug_table['n2lo_ct'] = output
+
+        return output
+
+
+    def fitfcn_n2lo_log(self, p, xi):
+        if self.model_info['include_fv']:
+            output = p['A_ll_g'] *xi['l']**2 *sf.fcn_I_m(xi['l'], p['L'], p['lam_chi'], 10)
+        else:
+            output = p['A_ll_g'] *xi['l']**2 *np.log(xi['l'])
+
+        if self.debug:
+            self.debug_table['n2lo_log'] = output
+
+        return output
+
+
+    def fitfcn_nlo_latt_ct(self, p, xi):
+        output = p['A_a'] *xi['a']
+
+        if self.debug:
+            self.debug_table['nlo_latt'] = output
+
+        return output
+
+
+    def fitfcn_n2lo_latt_ct(self, p, xi):
+        output = p['A_aa'] *xi['a']**2
+
+        if self.debug:
+            self.debug_table['n2lo_latt'] = output
+
+        return output
+
+
+    def buildprior(self, prior, mopt=None, extend=False):
+        return prior
+
+
+    def builddata(self, data):
+        return data[self.datatag]
+
+class Xi_st(lsqfit.MultiFitterModel):
+
+    def __init__(self, datatag, model_info, ens_mapping=None, observable=None, **kwargs):
+        super(model_interpolation, self).__init__(datatag)
+
+        # Model info
+        self.model_info = model_info
+        self.ens_mapping = ens_mapping
+        self.observable = observable
+
+
+    def fitfcn(self, p, fit_data=None, xi=None, latt_spacing=None, observable=None):
+        if fit_data is not None:
+            for key in fit_data.keys():
+                p[key] = fit_data[key]
+
+        for key in self.model_info['exclude']:
+            p[key] = 0
+
+
+        # Variables
+        if xi is None:
+            xi = {}
+        if 'l' not in xi:
+            xi['l'] = (p['mpi'] / p['lam_chi'])**2
+        if 's' not in xi:
+            xi['s'] = (2 *p['mk']**2 - p['mpi']**2) / p['lam_chi']**2
+
+        y_ch = self.fitfcn_lo_ct(p, xi, latt_spacing)
+
+        if 'a' not in xi:
+            if self.observable == 'w0':
+                xi['a'] =  1 / (2 *y_ch)**2
+            elif self.observable == 't0':
+                xi['a'] =  1 / (4 *y_ch)
+
+        # lo
+        #output = w0ch_a
+
+        # nlo
+        #output += w0ch_a *self.fitfcn_nlo_ct(p, xi)
+
+        # n2lo
+        #output += w0ch_a *self.fitfcn_n2lo_ct(p, xi)
+        #output += w0ch_a *self.fitfcn_n2lo_log(p, xi)
+
+        #return output
+
+        #print(np.sqrt(xi['a'] ))
+
+        #if isinstance(w0ch_a[0], gv._gvarcore.GVar):
+        #    print(gv.evalcorr([w0ch_a[0], self.fitfcn_lo_ct(p, latt_spacing)[0]]))
+
+        #output = 1 / (2 *np.sqrt(xi['a'])) *(
+        output = y_ch *(
+            + 1
+            + self.fitfcn_nlo_ct(p, xi)
+            + self.fitfcn_n2lo_ct(p, xi)
+            + self.fitfcn_n2lo_log(p, xi)
+        )
+
+        #print('good')
+        return output
+
+
+    def fitfcn_lo_ct(self, p, xi, latt_spacing=None):
+
+        if latt_spacing == 'a06':
+            output = p['c0a06']
+        elif latt_spacing == 'a09':
+            output= p['c0a09']
+        elif latt_spacing == 'a12':
+            output = p['c0a12']
+        elif latt_spacing == 'a15':
+            output = p['c0a15']
+
+        else:
+            output = xi['l'] *xi['s'] *0 # returns correct shape
+            for j, ens in enumerate(self.ens_mapping):
+                if ens[:3] == 'a06':
+                    output[j] = p['c0a06']
+                elif ens[:3] == 'a09':
+                    output[j] = p['c0a09']
+                elif ens[:3] == 'a12':
+                    output[j] = p['c0a12']
+                elif ens[:3] == 'a15':
+                    output[j] = p['c0a15']
+                else:
+                    output[j] = 0
+
+        return output
+
+
+    def fitfcn_nlo_ct(self, p, xi):
+        output = (
+            + p['k_l'] *xi['l'] 
+            + p['k_s'] *xi['s'] 
+            + p['k_a'] *xi['a']
+        )
+        return output
+
+
+    def fitfcn_n2lo_ct(self, p, xi):     
+        output = ( 
+            + p['k_aa'] *xi['a'] *xi['a']
+            + p['k_al'] *xi['a'] *xi['l']
+            + p['k_as'] *xi['a'] *xi['s']
+            + p['k_ll'] *xi['l'] *xi['l']
+            + p['k_ls'] *xi['l'] *xi['s']
+            + p['k_ss'] *xi['s'] *xi['s']
+        )
+        return output
+
+
+    def fitfcn_n2lo_log(self, p, xi):
+        output = p['k_ll_g'] *xi['l']**2 *np.log(xi['l'])
+        return output
+
+
+    def buildprior(self, prior, mopt=None, extend=False):
+        return prior
+
+
+    def builddata(self, data):
+        return data[self.datatag]
+
+
+
+
+
+        
--- xpt/non_analytic_functions.py
+++ xpt/non_analytic_functions.py
@@ -1,38 +1,69 @@
 import gvar as gv
 import numpy as np
 import scipy.special as ss
+import i_o
 
+LECs = i_o.InputOutput.get_data_phys_point('lam_chi')
+print(LECs)
+lam_chi = LECs['lam_chi']
+m_pi = LECs['m_pi']
+m_k = LECs['m_k']
+m_xi = LECs['m_xi']
+m_xi_st = LECs['m_xi_st']
+print(m_k)
+eps_pi = gv.mean(m_pi) / gv.mean(lam_chi) # set to zero in the chiral limit 
 
-# define lecs 
+# for Xi system, delta is mass splitting between Xi* and Xi
+delta = gv.mean(m_xi_st) - gv.mean(m_xi)
+print(delta)
 
-lam_chi = 4*np.pi*F_pi
-eps_pi
-eps_delta 
+#eps_pi = gv.mean(m_pi) / gv.mean(lam_chi) # set to zero in the chiral limit 
+eps_delta = delta / gv.mean(lam_chi)
+
 ### non-analytic functions that arise in extrapolation formulae for hyperon masses
-def fcn_R(x):
-    
-    if (x>0 & x<=1):
+
+def fcn_R(g):
+    if isinstance(g, gv._gvarcore.GVar):
+        x = gv.mean(g)
+        #print(x)
+
+        if (x>0 and x<=1):
         #a = np.sqrt(1-x)
-        R = np.sqrt(1-x) * np.log((1-np.sqrt(1-x))/(1+np.sqrt(1-x)))
-    elif x>1:
+            R = np.sqrt(1-x) * np.log((1-np.sqrt(1-x))/(1+np.sqrt(1-x)))
+        elif x>1:
         #b = np.sqrt(x-1)
-        R = 2*np.sqrt(x-1)*np.arctan(np.sqrt(x-1))
+            R = 2*np.sqrt(x-1)*np.arctan(np.sqrt(x-1))
+    #not gvar
+    else:
+        if (g>0 and g<=1):
+        #a = np.sqrt(1-x)
+            R = np.sqrt(1-g) * np.log((1-np.sqrt(1-g))/(1+np.sqrt(1-g)))
+        elif x>1:
+        #b = np.sqrt(x-1)
+            R = 2*np.sqrt(g-1)*np.arctan(np.sqrt(g-1))
+
+
     return R
+
+eps_pi_delta = eps_pi**2 / eps_delta**2
+#fcn_R(eps_pi_delta)
 
 
 def fcn_F(eps_pi, eps_delta, mu):
-    output = -eps_pi*(eps_delta**2 - eps_pi**2) * fcn_R(x=eps_pi**2/eps_delta**2) -
-    ((3/2) * eps_pi**2 * eps_delta * np.log(eps_pi**2 * (lam_chi**2)/(mu**2) - eps_delta**3 *
-    np.log((4*eps_delta**2)/ eps_pi**2) ))
+
+    output = -1*eps_delta*(eps_delta**2 - eps_pi**2) * fcn_R(eps_pi**2 / eps_delta**2)
+    output = output - ((3/2) * eps_pi**2 * eps_delta * np.log(eps_pi**2 * (lam_chi**2)/(mu**2)))
+    output = output - (eps_delta**3 * np.log((4*eps_delta**2) / (eps_pi**2)))
     
     return output
 
 
 def fcn_J(eps_pi, eps_delta, mu):
-    output = eps_pi**2 * np.log(eps_pi**2 * (lam_chi**2)/(mu**2) + 2*eps_delta**2 * np.log((4*eps_delta**2)/ eps_pi**2) ) +
-    2*eps_delta**2 * fcn_R(x=eps_pi**2/eps_delta**2)
+    out = eps_pi**2 * np.log(eps_pi**2 * (lam_chi**2)/(mu**2))
+    out = out +  2*eps_delta**2 * np.log((4*eps_delta**2)/ eps_pi**2)
+    out = out +  2*eps_delta**2 * fcn_R(eps_pi**2/eps_delta**2)
 
-    return output
+    return out
 
 
     
